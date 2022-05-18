8b2890a3d6ce17a902ad0c67de969ffc4f89dd24
cateruu <cater981pl@gmail.com>
2022-05-18 17:46:30 +0200

    Activities creation

--- scripts/drawActivities.js
+++ scripts/drawActivities.js
@@ -6,32 +6,47 @@ const doneContainer = document.getElementById('done');
 
 let storage = JSON.parse(localStorage.getItem('activities')) || [];
 
-if (storage) {
-  for (let activity of storage) {
-    // create single activity
-    let div = document.createElement('div');
-    div.classList.add('activity', 'draggable');
-    div.innerHTML = `<span>${activity.value}</span>`;
-    
-    // buttons container
-    let buttons = document.createElement('div');
-    buttons.classList.add('buttons');
-    div.append(buttons);
+for (let activity of storage) {
+  // create single activity
+  let div = document.createElement('div');
+  div.classList.add('activity', 'draggable');
+  div.innerHTML = `<span>${activity.value}</span>`;
 
-    // done and delete buttons
-    let checkmark = document.createElement('button');
-    let bin = document.createElement('button');
-    checkmark.innerHTML = '<img src="./images/done.png" alt="done" title="done" height="25px">';
-    bin.innerHTML = '<img src="./images/delete.png" alt="delete" title="delete" height="25px">';
-    buttons.append(checkmark, bin);
+  createActivity(activity, div);
+}
 
-    // time
-    let time = document.createElement('span');
-    time.classList.add('time');
-    let date = new Date(activity.id);
-    time.innerHTML = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
-    div.prepend(time);
+function createActivity(activity, container) {
+  // buttons container
+  let buttons = document.createElement('div');
+  buttons.classList.add('buttons');
+  container.append(buttons);
 
-    defaultContainer.append(div);
+  // done and delete buttons
+  let checkmark = document.createElement('button');
+  let bin = document.createElement('button');
+  checkmark.innerHTML = '<img src="./images/done.png" alt="done" title="done" height="25px">';
+  bin.innerHTML = '<img src="./images/delete.png" alt="delete" title="delete" height="25px">';
+  buttons.append(checkmark, bin);
+
+  // time
+  let time = document.createElement('span');
+  time.classList.add('time');
+  let date = new Date(activity.id);
+  time.innerHTML = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+  container.prepend(time);
+
+  switch(activity.state) {
+    case 'default':
+      defaultContainer.append(container);
+      break;
+    case 'important':
+      importantContainer.append(container);
+      break;
+    case 'done':
+      doneContainer.append(container);
+      break;
+    default:
+      console.log(new Error(`Wrong state in activity id: "${activity.id}"!`));
+      break;
   }
 }
